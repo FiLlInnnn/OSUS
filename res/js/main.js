684ba7b34632592469feae680b603e060392cf2f
filip <filip.vorisek@volny.cz>
2023-11-03 09:27:47 +0100

    first

---
+++ res/js/main.js
@@ -0,0 +1,40 @@
+//      params - parmetry
+/*function tomas(item, item2) {
+    console.log("Nesu: " + item + ", " + item2)
+};
+//      args - argumenty
+tomas("mys", "pocitac");
+tomas("notebook", "monitor");
+
+const ttomas = (item, item2) => {
+    console.log(`Nesu: ${item}, ${item2}`);
+}
+
+tomas("mys", "pocitac");
+tomas("notebook", "monitor");*/
+// 9 + shift = ()
+/*function play(song, artist) {
+    console.log(`Playing: ${song} by ${artist}`);
+}
+const plaz = (song, artist) => {
+    console.log(`Playing: ${song} by ${artist}`);
+}
+play("Never gonna give you up", "Rick Astley")
+plaz("HAFO", "Frank Wild")*/
+
+/*function sum(a, b) {
+    return a + b;
+}
+
+let resultA = sum(5, 7);
+let resultB = sum(7, 8);
+let result = sum(resultA, resultA);
+console.log(result);
+
+const ssum = (a, b) => a +b;
+
+let rresultA = sum(12, 7);
+let rresultB = sum(7, 16);
+let rresult = sum(rresultA, rresultA);
+console.log(rresult);*/
+
